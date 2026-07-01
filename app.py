40e1f40b82eba2167dddbcbb400e53f253bbb195
sintuvez31-web <sintuvez31@gmail.com>
2026-07-01 18:45:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,20 +4,15 @@ import requests
 app = Flask(__name__)
 app.secret_key = 'cok_gizli_anahtar_123'
 
-# BURAYI KENDİ BİLGİLERİNLE GÜNCELLE
+# Buraya kendi bilgilerini gir
 BIN_ID = '6a452d96f5f4af5e294e2344'
-MASTER_KEY = '$2a$10$Vm/z9QlARXZ/0VCmOwuF7.0Y0lx4OMxSR9b8gTy4Sv5aR4U3A822O'
-HEADERS = {'X-Master-Key': MASTER_KEY, 'Content-Type': 'application/json'}
+MASTER_KEY = 'BURAYA_KENDI_MASTER_KEYINI_YAZ' 
 
 def load_data():
     try:
         response = requests.get(f'https://api.jsonbin.io/v3/b/{BIN_ID}/latest', headers={'X-Master-Key': MASTER_KEY})
         return response.json().get('record', {})
     except: return {}
-
-@app.route('/')
-def home():
-    return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -39,20 +34,15 @@ def login():
         
     return render_template('login.html', hata=hata)
 
-@app.route('/dashboard')
-def dashboard():
-    if 'username' not in session or session['username'] == 'admin': return redirect(url_for('login'))
-    return render_template('dashboard.html', user_data=load_data().get(session['username']))
-
 @app.route('/admin')
 def admin_dashboard():
     if session.get('username') != 'admin': return redirect(url_for('login'))
     return render_template('admin.html', students=load_data())
 
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('login'))
+@app.route('/dashboard')
+def dashboard():
+    if 'username' not in session or session['username'] == 'admin': return redirect(url_for('login'))
+    return render_template('dashboard.html', user_data=load_data().get(session['username']))
 
 if __name__ == '__main__':
     app.run(debug=True)
